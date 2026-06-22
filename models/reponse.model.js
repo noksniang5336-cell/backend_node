49b1dcabefb910f8a91218a35522c600dd79c452
noksniang5336-cell <noksniang5336@gmail.com>
2026-06-22 15:09:19 +0000

    commit

--- models/reponse.model.js
+++ models/reponse.model.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const reponseSchema = new mongoose.Schema({
-    contenu: {
-        type: String,
-        required: true
-    },
-    question: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Question', // Optionnel : lie au modèle Question si tu en as un
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  questionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Question',
+    required: true
+  },
+  contenu: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Reponse', reponseSchema);
