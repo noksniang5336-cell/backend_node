ccc759d48ddbc062d651e70b015706aad568d308
noksniang5336-cell <noksniang5336@gmail.com>
2026-06-26 14:48:12 +0000

    commit

--- models/question.js
+++ models/question.js
@@ -1,33 +1,19 @@
 const mongoose = require("mongoose");
 
-
-const questionSchema = mongoose.Schema({
-
- titre:{
-   type:String,
-   required:true
- },
-
- description:{
-   type:String,
-   required:true
- },
-
- tags:{
-   type:[String],
-   default:[]
- },
-
-
- createdAt:{
-   type:Date,
-   default:Date.now
- }
-
+const questionSchema = new mongoose.Schema({
+  titre: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
 
-module.exports = mongoose.model(
-"Question",
-questionSchema
-);
+module.exports = mongoose.models.Question || mongoose.model("Question", questionSchema);
