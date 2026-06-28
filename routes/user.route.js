6aac81a6472923d63464cb675945dd962a999de4
noksniang5336-cell <noksniang5336@gmail.com>
2026-06-28 21:48:27 +0000

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { inscription, connexion, profil } = require('../controllers/user.controller');
 
+// 💡 AJOUT DU MIDDLEWARE D'AUTHENTIFICATION 
+// (Ajustez le chemin '../middlewares/auth.js' si votre fichier s'appelle autrement ou est placé ailleurs)
+const auth = require('../middlewares/auth'); 
 
 const router = express.Router();
+
 router.post("/inscription", inscription);
 router.post("/connexion", connexion);
 
-
-// récupérer le profil utilisateur
-router.get("/profil",  auth, profil);
-
-
+// Récupérer le profil utilisateur (Désormais 'auth' est bien défini !)
+router.get("/profil", auth, profil);
 
 module.exports = router;
