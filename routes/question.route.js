276c7dca6022c17d6f41ebdee2bb43eb074c3680
noksniang5336-cell <noksniang5336@gmail.com>
2026-06-30 11:49:51 +0000

    commit

--- routes/question.route.js
+++ routes/question.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Question = require("../models/Question");
+const Question = require("../models/question");
 
 
 // GET toutes les questions
